203f65224b61ff1965ca91f290c7530d41dee8dc
Francisco Moya <francisco.moya@uclm.es>
2016-04-02 23:28:27 +0200

    Fixed bug in test_connector

--- c/reactor/test/test_connector.c
+++ c/reactor/test/test_connector.c
@@ -1,4 +1,5 @@
 #include <reactor/reactor.h>
+#include <reactor/exception.h>
 #include <reactor/socket_handler.h>
 #include <reactor/timeout_handler.h>
 #include <stdio.h>
@@ -12,21 +13,35 @@ void handler(event_handler* ev)
     printf("Got [%s]\n", buf);
 }
 
-int main()
+void test_connector()
 {
     reactor* r = reactor_new();
     connector* c = connector_new("localhost", "8888", handler);
     reactor_add(r, (event_handler*)c);
 
+    /* c puede ser liberado en cualquier momento. El timeout tiene que
+       usar una variable que no pueda cambiar */
+    connector c_aux = *c;
+    
     void producer(event_handler* ev)
     {
-	static int i = 1;
+	static int i;
 	char buf[128];
 	snprintf(buf, 128, "Prueba %d", i++);
-	connector_send(c, buf, strlen(buf));
+	connector_send(&c_aux, buf, strlen(buf));
     }
 
     reactor_add(r, (event_handler*)timeout_handler_new(1000, producer));
     reactor_run(r);
-    reactor_destroy(r);
+}
+
+int main()
+{
+    exception e;
+    Try {
+	test_connector();
+    }
+    Catch(e) {
+	perror(e.what);
+    }
 }
