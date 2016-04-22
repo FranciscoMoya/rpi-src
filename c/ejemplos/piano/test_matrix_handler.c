5467d3b750cc260becd7daefd0dd3edba5cf2047
Francisco Moya <francisco.moya@uclm.es>
2016-04-22 04:35:21 +0200

    Small bugs

--- c/ejemplos/piano/test_matrix_handler.c
+++ c/ejemplos/piano/test_matrix_handler.c
@@ -6,15 +6,17 @@
 static void press(matrix_handler* ev, int key) { printf("Press %d\n", key); }
 static void release(matrix_handler* ev, int key) { printf("Release %d\n", key); }
 
+#define NELEMS(x) (sizeof(x)/sizeof(x[0]))
+
 int main()
 {
-    int rows[] = { 18, 23, 24, 25 };
+    int rows[] = { 4, 17, 27, 22 };
     int cols[] = { 18, 23, 24, 25 };
 
     wiringPiSetupGpio();
     reactor* r = reactor_new();
-    reactor_add(r, (event_handler*) matrix_handler_new(rows, sizeof(rows),
-						       cols, sizeof(cols),
+    reactor_add(r, (event_handler*) matrix_handler_new(rows, NELEMS(rows),
+						       cols, NELEMS(cols),
 						       press, release));
     reactor_run(r);
     return 0;
